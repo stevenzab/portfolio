26e4c0a6c1d18a5e4b6c6f38289e536ee65b936d
Steven ZABANDITH <steven.zabandith@epitech.eu>
2022-12-18 19:47:21 +0100

    [ADD] begin project navbar nad homepage

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Homepage from './styles/pages/homepage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ my-app/src/styles/pages/homepage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../components/navbar';
+
+function Homepage() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Homepage</h1>
+        </div>
+    );
+}
+
+export default Homepage;
