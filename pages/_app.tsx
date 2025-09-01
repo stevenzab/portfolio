5bb14eff01b1da2942e4a2519cbf597de21ccbd8
stevenzab <steven.zabandith@epitech.eu>
2025-09-01 10:18:22 +0200

    [ADD] integrate @vercel/analytics for enhanced performance tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../app/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/next"
 import RootLayout from "@/components/layout";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <RootLayout>
       <Component {...pageProps} />
+      <Analytics />
     </RootLayout>
   );
 };
